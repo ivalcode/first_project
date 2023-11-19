bbf9ffa79c3f7ccb01d961f8772e6cf5ec9ef0a7
ivalcode <132266756+ivalcode@users.noreply.github.com>
2023-11-19 13:54:12 +0400

    переданные значения для переменных шаблона index.html
    Что сделать ДЗ и второе задание нового урока

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', path_css="/static/css/style.css",
+                        path_img="/static/img/man.webp")
 
 
 @app.route('/calc')
